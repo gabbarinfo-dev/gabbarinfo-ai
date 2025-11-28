f406e3c08a9f52ab9c74b882d9e285902df724f4
gabbarinfo-dev <ndantare@gmail.com>
2025-11-28 19:43:47 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 const SYSTEM_PROMPT = `
