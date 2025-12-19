28be1629d1d1e87c7041bd0a918339627d3fcddb
gabbarinfo-dev <ndantare@gmail.com>
2025-12-20 04:04:15 +0530

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -32,6 +32,11 @@ export const authOptions = {
         },
       },
     }),
+     FacebookProvider({
+    clientId: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  }),
+],
   ],
 
   secret: process.env.NEXTAUTH_SECRET,
