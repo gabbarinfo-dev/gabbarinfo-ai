639a5a2de17d78d78b572cd47ae3c32d9b4ad73f
gabbarinfo-dev <ndantare@gmail.com>
2025-12-24 05:46:03 +0530

    Create disconnect.js

---
+++ pages/api/meta/disconnect.js
@@ -0,0 +1,22 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]";
+import { supabaseServer } from "../../../lib/supabaseServer";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  const session = await getServerSession(req, res, authOptions);
+
+  if (!session?.user?.email) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  await supabaseServer
+    .from("meta_connections")
+    .delete()
+    .eq("email", session.user.email);
+
+  return res.json({ success: true });
+}
