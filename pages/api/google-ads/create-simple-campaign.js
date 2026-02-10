bbb0a088780e33da25c47094ce4998acc5d2701f
gabbarinfo-dev <ndantare@gmail.com>
2026-02-10 15:50:39 +0530

    Update create-simple-campaign.js

--- pages/api/google-ads/create-simple-campaign.js
+++ pages/api/google-ads/create-simple-campaign.js
@@ -29,6 +29,7 @@ if (!SUPABASE_URL || !SUPABASE_SERVICE_ROLE_KEY) {
 const supabaseServer = createClient(SUPABASE_URL || "", SUPABASE_SERVICE_ROLE_KEY || "");
 
 export default async function handler(req, res) {
+  console.log("METHOD:", req.method);
   // CORS HEADERS
 res.setHeader("Access-Control-Allow-Origin", "*");
 res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
