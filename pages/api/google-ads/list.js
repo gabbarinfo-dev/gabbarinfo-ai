bca6dc9a9a818a650ece0db7266d4ddacf234957
gabbarinfo-dev <ndantare@gmail.com>
2026-02-11 11:46:42 +0530

    Update list.js

--- pages/api/google-ads/list.js
+++ pages/api/google-ads/list.js
@@ -5,18 +5,23 @@ export default async function handler(req, res) {
   try {
     const token = await getAccessToken();
 
-    const customerId = process.env.GOOGLE_ADS_CLIENT_ACCOUNT_ID;
-    const loginCustomerId = process.env.GOOGLE_ADS_LOGIN_CUSTOMER_ID;
+   const customerId = process.env.GOOGLE_ADS_CLIENT_ACCOUNT_ID;
+const loginCustomerId = process.env.GOOGLE_ADS_LOGIN_CUSTOMER_ID;
 
-    const query = `
-  SELECT customer.id, customer.descriptive_name
-  FROM customer
+const query = `
+  SELECT
+    campaign.id,
+    campaign.name,
+    campaign.status
+  FROM campaign
+  LIMIT 20
 `;
 
-
 const response = await axios.post(
-  `https://googleads.googleapis.com/v16/customers/${customerId}/googleAds:searchStream`,
-  { query },
+  `https://googleads.googleapis.com/v16/customers/${customerId}/googleAds:search`,
+  {
+    query
+  },
   {
     headers: {
       Authorization: `Bearer ${token}`,
